844c01ea34368a0980847d41a32e38612bcef11f
Axel Dahlberg <git@valleymnt.com>
2021-06-22 22:26:24 +0200

    config generation now works

--- kitten_options_utils.py
+++ kitten_options_utils.py
@@ -19,3 +19,19 @@ def parse_map(val: str) -> Iterable[KittensKeyDefinition]:
     x = parse_kittens_key(val, args_funcs)
     if x is not None:
         yield x
+
+
+# TODO what are these functions supposed to do?
+@func_with_args('move')
+def move(func, rest):
+    return func, rest
+
+
+@func_with_args('scroll')
+def scroll(func, rest):
+    return func, rest
+
+
+@func_with_args('select')
+def select(func, rest):
+    return func, rest
