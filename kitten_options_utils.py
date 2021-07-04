9d377491f7500f92ca0a3fb2c93b9d0d461dd9b3
Axel Dahlberg <git@valleymnt.com>
2021-07-04 21:40:18 +0200

    fix(util): added back correct parsers

--- kitten_options_utils.py
+++ kitten_options_utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Sequence, Tuple, Iterable
+from typing import Any, Sequence, Tuple, Iterable, Callable
 
 from kitty.conf.utils import KittensKeyDefinition, key_func, parse_kittens_key
 
@@ -12,17 +12,41 @@ def parse_map(val: str) -> Iterable[KittensKeyDefinition]:
         yield x
 
 
-# TODO what are these functions supposed to do?
+def parse_region_type(region_type: str) -> str:
+    result = region_type.lower()
+    assert result in ['stream', 'columnar']
+    return result
+
+
+def parse_direction(direction: str) -> str:
+    direction_lc = direction.lower()
+    assert direction_lc in ['left', 'right', 'up', 'down',
+                            'page up', 'page down',
+                            'first', 'first nonwhite',
+                            'last nonwhite', 'last',
+                            'top', 'bottom',
+                            'word left', 'word right']
+    return direction_lc.replace(' ', '_')
+
+
+def parse_scroll_direction(direction: str) -> str:
+    result = direction.lower()
+    assert result in ['up', 'down']
+    return result
+
+
 @func_with_args('move')
-def move(func, rest):
-    return func, rest
+def move(func: Callable, direction: str) -> Tuple[Callable, str]:
+    return func, parse_direction(direction)
 
 
 @func_with_args('scroll')
-def scroll(func, rest):
-    return func, rest
+def scroll(func: Callable, direction: str) -> Tuple[Callable, str]:
+    return func, parse_scroll_direction(direction)
 
 
 @func_with_args('select')
-def select(func, rest):
-    return func, rest
+def select(func: Callable, args: str) -> Tuple[Callable, Tuple[str, str]]:
+    region_type, direction = args.split(' ', 1)
+    return func, (parse_region_type(region_type),
+                  parse_direction(direction))
