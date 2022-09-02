d7a6ff6aa36f0e0b283e284965ffaaf606e9fbf4
Albert Kim <alkim@alkim.org>
2022-09-01 23:13:43 -0700

    Cleared up conflicting documentation

--- kitten_options_definition.py
+++ kitten_options_definition.py
@@ -104,8 +104,7 @@ map('SelectColumnar word left',      'select_columnar Alt+Ctrl+Left  select colu
 map('SelectColumnar word right',     'select_columnar Alt+Ctrl+Right select columnar word right', long_text=long_text)
 
 long_text = '''
-Keys to enable vim-like modal selecting.
-These keys are not enabled by default.'''
+Keys to enable vim-like modal selecting.'''
 map('SetMode visual', 'set_mode v                   set_mode visual')
 map('SetMode block',  'set_mode Ctrl+v              set_mode block')
 map('SetMode normal', 'set_mode Ctrl+LeftBracket    set_mode normal', long_text=long_text)
